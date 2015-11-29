3d06def685c53bda5fdd938aaa5856e69d6ab01f
Swapnil Sankla <ssankla@seek.com.au>
2015-11-29 14:43:48 +0530

    Initial commit

---
+++ UsingAutofac/StartUp.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Autofac;
+using Autofac.Core;
+
+namespace UsingAutofac
+{
+    public class StartUp
+    {
+        private IContainer _container;
+
+        public static void Main()
+        {
+            StartUp startup = new StartUp();
+            startup._container = BuildContainer(startup).Build();
+            startup.WriteDate();
+            Console.ReadLine();
+        }
+
+        private static ContainerBuilder BuildContainer(StartUp startup)
+        {
+            var builder = new ContainerBuilder();
+            builder.RegisterType<ConsoleOutput>().As<IOutput>();
+            builder.RegisterType<TodayDateWriter>().AsSelf().As<IDateWriter>();
+            return builder;
+        }
+
+        private void WriteDate()
+        {
+            using (var scope = _container.BeginLifetimeScope())
+            {
+                var writer = scope.Resolve<IDateWriter>();
+                writer.WriteDate();
+            }
+        }
+    }
+}
---
+++ UsingAutofac/TodayDateWriter.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UsingAutofac
+{
+    class TodayDateWriter : IDateWriter
+    {
+        private IOutput _output;
+
+        public TodayDateWriter(IOutput output)
+        {
+            _output = output;
+        }
+
+        public void WriteDate()
+        {
+            _output.Write(DateTime.Now.Date.ToString());
+        }
+    }
+}
